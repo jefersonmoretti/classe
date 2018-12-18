0c8f583a5f3a06a2633a2f922eafb6a952b2c581
jefersonmoretti <jefemoretti@gmail.com>
2018-12-18 19:42:16 -0200

    Criando classe cliente

---
+++ src/main/java/br/com/git/GitHub3/model/Cliente.java
@@ -0,0 +1,27 @@
+package br.com.git.GitHub3.model;
+
+public class Cliente {
+	private String nome;
+	private String sobreNome;
+	private int idade;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+}
